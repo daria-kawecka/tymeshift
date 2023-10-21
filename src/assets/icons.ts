ac9c60907243aea4bf8d286a6e6c1c0912e4e951
daria-kawecka <daria.kawecka28@gmail.com>
2023-10-21 16:26:28 +0200

    Configure icons

---
+++ src/assets/icons.ts
@@ -0,0 +1,16 @@
+import Timezone from 'assets/Timezone.svg';
+import Users from 'assets/Users.svg';
+import Views from 'assets/Views.svg';
+import Edit from 'assets/Edit.svg';
+import Close from 'assets/Close.svg';
+
+export const ICONS = {
+  timezone: {
+    icon: Timezone,
+    name: 'timezone',
+  },
+  users: { icon: Users, name: 'users' },
+  views: { icon: Views, name: 'views' },
+  edit: { icon: Edit, name: 'edit' },
+  close: { icon: Close, name: 'close' },
+};
